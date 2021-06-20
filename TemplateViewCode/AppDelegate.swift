32c3fa0e998f3822180a2aa5aaeeea9df463f8ef
diogenesNegreiros <diogenesdesouza@yahoo.com.br>
2021-06-20 09:36:14 -0300

    fix rootViewController in appDelegate

--- TemplateViewCode/AppDelegate.swift
+++ TemplateViewCode/AppDelegate.swift
@@ -9,27 +9,31 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let viewModel = LoginViewModel()
+        let vc = LoginViewController(viewModel: viewModel)
+        vc.view.backgroundColor = .white
+        self.window?.rootViewController = UINavigationController(rootViewController: vc)
+        self.window?.makeKeyAndVisible()
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-
+        
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     @available(iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-
+        
     }
-
-
 }
 
--- TemplateViewCode/SceneDelegate.swift
+++ TemplateViewCode/SceneDelegate.swift
@@ -9,15 +9,13 @@ import UIKit
 
 @available(iOS 13.0, *)
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-
+        
         if let widowScene = scene as? UIWindowScene{
             let viewModel = LoginViewModel()
-            
             let window = UIWindow (windowScene: widowScene)
             let vc = LoginViewController(viewModel: viewModel)
             vc.view.backgroundColor = .white
@@ -26,9 +24,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             window.makeKeyAndVisible()
         }
     }
-
-  
-
-
 }
 
